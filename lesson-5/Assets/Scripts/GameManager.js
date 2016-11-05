65502a5dbfc0ade323bc7169d727c8bbd28018fc
James Tsay <tsay@adobe.com>
2016-11-05 13:12:18 -0700

    Add lesson 5

---
+++ lesson-5/Assets/Scripts/GameManager.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var ball : Transform;
+var floor : Transform;
+
+
+function Start () {
+  var columns = 10;
+  var rows = 10;
+  var colors = [Color.green, Color.red, Color.blue];
+
+  var pos = Vector3(floor.position.x - Mathf.Floor(columns/2)*ball.localScale.x, 
+                    floor.position.y + floor.localScale.y / 2 + ball.localScale.y / 2);
+  var instance : Transform;
+  for (var i=0; i<columns; i++) {
+    for (var j=0; j<rows; j++) {
+      var ballPos = Vector3(pos.x + i*ball.localScale.x, pos.y + j*ball.localScale.y);
+  	  instance = Instantiate(ball, ballPos, Quaternion.identity);
+  	  instance.gameObject.GetComponent.<Renderer>().material.color = colors[Random.Range(0, colors.length)];
+  	}
+  }
+}
+
+function Update () {
+  if (Input.GetMouseButtonDown(0)) {
+    // Get a ray at the click position 
+    var hit: RaycastHit;
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    if (Physics.Raycast(ray, hit)) {
+      Destroy(hit.transform.gameObject);
+    }
+  }
+}
